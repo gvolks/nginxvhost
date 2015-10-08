b9e6bf20c6cc4acdd31a971e8f0fbaf12638df12
volks <ger.cugliandolo@estoes.me>
2015-10-08 18:34:33 -0300

    fix

--- includes/config.php
+++ includes/config.php
@@ -1,4 +1,6 @@
 <?php
+header('Content-Type: text/html; charset=UTF-8');
+
 define("BASE_URL", "http://localhost/nginx");
 define("PASSWORD", "p3p3p3");
 define('NGINX_SITES','/etc/nginx/sites-available');
